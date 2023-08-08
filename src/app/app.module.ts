e3c79f5d86f1630a62c44092fe827c701ac960f9
Dicky <dickyfajardarmawan@gmail.com>
2023-08-08 15:35:14 +0700

    first update

---
+++ src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+// Import Firebase modules + environment
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from '../environments/environment';
+import { AddStudentComponent } from './add-student/add-student.component';
+import { EditStudentComponent } from './edit-student/edit-student.component';
+import { StudentListComponent } from './student-list/student-list.component';
+// Import below modules for NGX Toastr
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/register-student', pathMatch: 'full' },
+  { path: 'register-student', component: AddStudentComponent },
+  { path: 'view-students', component: StudentListComponent },
+  { path: 'edit-student/:id', component: EditStudentComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddStudentComponent,
+    EditStudentComponent,
+    StudentListComponent
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,RouterModule.forRoot(routes),
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
+    FormsModule,
+    NgxPaginationModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot() // ToastrModule added
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyBbDEAp8n9vJQa0GH_VFmNF122ai9Rk3cg",
+        authDomain: "my-app-name-c2545.firebaseapp.com",
+        databaseURL: "https://my-app-name-c2545-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "my-app-name-c2545",
+        storageBucket: "my-app-name-c2545.appspot.com",
+        messagingSenderId: "642454947492",
+        appId: "1:642454947492:web:3471ec62160f21ca7a8831",
+        measurementId: "G-42P1VVKKS3"
+    }
+};
